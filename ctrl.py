46f578caee38ae45b4e1b33f3cb43f1ea3b03924
[kim] <secondsung23@gmail.com>
2026-01-07 23:44:29 +0900

    Add power operator to combobox and repair error

--- ctrl.py
+++ ctrl.py
@@ -1,19 +1,18 @@
 class Control:
-
     def __init__(self, view):
         self.view = view
         self.connectSignals()
-
+        
     def calculate(self):
         pass
-
+        
     def connectSignals(self):
-        self.view.btn1.clicked.connect(self.view.activateMessage)
+        self.view.btn1.clicked.connect(self.calculate)
         self.view.btn2.clicked.connect(self.view.clearMessage)
-
+        
     def sum(self, a, b):
         try:
-            return str(a+b)
+            return a+b
         except:
-             return "Calculation Error"
+            return "Calculation Error"
 
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
-from ui import View #ui.py의 View 클래스 추가
-from ctrl import Control #ctrl.py의 Control 클래스 추가
-from PyQt5.QtWidgets import QApplication
-    
-def main(): #프로그램 실행(Application) 관련 내용 함수화
-    app = QApplication(sys.argv)
-    view = View()
-    Control(view = view)
+from ui import View
+from ctrl import Control
+from PyQt5.QtWidgets import QApplication        
+
+def main():
+    app=QApplication(sys.argv)
+    view=View()
+    Control(view=view)
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
